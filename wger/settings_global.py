9d274d73601ffba926585c725906035218f48ccc
josiah nyarega <josiah.mokobo@andela.com>
2017-08-28 13:40:08 +0300

    [FIX] Update static root path

--- wger/settings_global.py
+++ wger/settings_global.py
@@ -319,7 +319,7 @@ THUMBNAIL_ALIASES = {
 # Django compressor
 #
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(SITE_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # The default is not DEBUG, override if needed
