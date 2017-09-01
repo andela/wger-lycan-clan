e5eb34ff2c3d917b03e0298c1b1a738d080d606e
Mbaka <salliedailey@gmail.com>
2017-09-01 11:37:04 +0300

    [FIX] fix lint errors on file

--- wger/core/static/js/user_list.js
+++ wger/core/static/js/user_list.js
@@ -1,21 +1,21 @@
 function filters() {
+  var i;
+  var td;
   var input = $('#select_filter').val();
   var filter = input.toUpperCase();
   var table = document.getElementById('main_member_list');
   var tr = table.getElementsByTagName('tr');
 
-  for (var i = 0; i < tr.length; i++) {
-    var td = tr[i].getElementsByTagName('td')[4];
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName('td')[4];
     if (filter === 'ALL') {
       tr[i].style.display = '';
     } else {
-      if (td) {
         if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
           tr[i].style.display = '';
         } else {
           tr[i].style.display = 'none';
         }
-      }
     }
   }
 }
