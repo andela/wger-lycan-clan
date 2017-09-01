8cc488567543311a3cb8e35bbbf050a9a574ce72
Mbaka <salliedailey@gmail.com>
2017-09-01 11:09:45 +0300

    [CHORE] fix lint errors

--- wger/core/static/js/user_list.js
+++ wger/core/static/js/user_list.js
@@ -1,16 +1,12 @@
 function filters() {
-  var input;
-  var filter;
-  var table;
-  var tr;
-  input = $('#select_filter').val();
-  filter = input.toUpperCase();
-  table = document.getElementById('main_member_list');
-  tr = table.getElementsByTagName('tr');
+  var input = $('#select_filter').val();
+  var filter = input.toUpperCase();
+  var table = document.getElementById('main_member_list');
+  var tr = table.getElementsByTagName('tr');
 
   for (var i = 0; i < tr.length; i++) {
     var td = tr[i].getElementsByTagName('td')[4];
-    if (filter == 'ALL') {
+    if (filter === 'ALL') {
       tr[i].style.display = '';
     } else {
       if (td) {
@@ -22,5 +18,4 @@ function filters() {
       }
     }
   }
-
 }
