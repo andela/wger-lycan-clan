c7e2af5427b2f8cd513c1940bc1d0a543314c53d
josiah nyarega <josiah.mokobo@andela.com>
2017-09-01 12:32:11 +0300

    [BUG] Fix lint errors

--- wger/core/static/js/user_list.js
+++ wger/core/static/js/user_list.js
@@ -1,21 +1,27 @@
 function filters() {
-  var i;
-  var td;
-  var input = $('#select_filter').val();
-  var filter = input.toUpperCase();
-  var table = document.getElementById('main_member_list');
-  var tr = table.getElementsByTagName('tr');
+  var input;
+  var  filter;
+  var  table;
+  var  tr;
 
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName('td')[4];
-    if (filter === 'ALL') {
+  input = $('#select_filter').val();
+  filter = input.toUpperCase();
+  table = document.getElementById('main_member_list');
+  tr = table.getElementsByTagName('tr');
+
+  for (var i = 0; i < tr.length; i++) {
+    var td = tr[i].getElementsByTagName('td')[4];
+    console.log(td);
+    if (filter == 'ALL') {
+      $('.Inactive').css('background', 'beige');
       tr[i].style.display = '';
+      
+    } else if (td && (td.innerHTML.toUpperCase().indexOf(filter) > -1)) {
+            tr[i].style.display = '';
     } else {
-        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = '';
-        } else {
-          tr[i].style.display = 'none';
-        }
-    }
+            tr[i].style.display = 'none';
+            console.log('No Data Tables');
+    }  
   }
 }
+console.log(filters());
