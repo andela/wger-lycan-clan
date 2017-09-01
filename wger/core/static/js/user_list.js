1c5726f6a252ccedff6191b9bb53bea7b687b52f
josiah nyarega <josiah.mokobo@andela.com>
2017-09-01 12:41:17 +0300

    [FIX]  Fix identation lint errors

--- wger/core/static/js/user_list.js
+++ wger/core/static/js/user_list.js
@@ -1,27 +1,26 @@
 function filters() {
   var input;
-  var  filter;
-  var  table;
-  var  tr;
+  var filter;
+  var table;
+  var tr;
+  var td;
+  var i;
 
   input = $('#select_filter').val();
   filter = input.toUpperCase();
   table = document.getElementById('main_member_list');
   tr = table.getElementsByTagName('tr');
 
-  for (var i = 0; i < tr.length; i++) {
-    var td = tr[i].getElementsByTagName('td')[4];
-    console.log(td);
-    if (filter == 'ALL') {
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName('td')[4];
+    if (filter === 'ALL') {
       $('.Inactive').css('background', 'beige');
-      tr[i].style.display = '';
-      
+      tr[i].style.display = '';   
     } else if (td && (td.innerHTML.toUpperCase().indexOf(filter) > -1)) {
-            tr[i].style.display = '';
+        tr[i].style.display = '';
     } else {
-            tr[i].style.display = 'none';
-            console.log('No Data Tables');
-    }  
+        tr[i].style.display = 'none';
+    } 
   }
 }
 console.log(filters());
