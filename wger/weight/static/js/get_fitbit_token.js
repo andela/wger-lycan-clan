1c7675802421b39d00c411ff95427d9b6e3f5d6c
Jimmy <jameskinyua590@gmail.com>
2017-09-18 13:30:24 +0300

    [CHORE] Get the fitbit token from the url bar

---
+++ wger/weight/static/js/get_fitbit_token.js
@@ -0,0 +1,10 @@
+var url = window.location.href;
+var origin = window.location.origin;
+
+try {
+    window.location.replace(
+        origin + '/en/weight/fitbit/' + url.split('#')[1].split('=')[1].split('&')[0]
+    );
+} catch (error) {
+    
+}
