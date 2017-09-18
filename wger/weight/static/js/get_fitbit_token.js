5f5defd0e4b83a2863353cb539ced828bdc38975
Jimmy <jameskinyua590@gmail.com>
2017-09-18 15:10:56 +0300

    [FIX] Rename the getter url to getweight

--- wger/weight/static/js/get_fitbit_token.js
+++ wger/weight/static/js/get_fitbit_token.js
@@ -3,7 +3,7 @@ var origin = window.location.origin;
 
 try {
     window.location.replace(
-        origin + '/en/weight/fitbit/' + url.split('#')[1].split('=')[1].split('&')[0]
+        origin + '/en/weight/getweight/' + url.split('#')[1].split('=')[1].split('&')[0]
     );
 } catch (error) {
     console.log(error);
