dec3a227f108d92d52c3275bbf4b83750a6930ef
Jimmy <jameskinyua590@gmail.com>
2017-09-20 15:53:14 +0300

    [CHORE] Delete unnecessary file

--- wger/exercises/static/js/get_fitbit_token.js
+++
@@ -1,9 +0,0 @@
-var origin = window.location.origin;
-
-try {
-    window.location.replace(
-        origin + '/en/exercise/getexercises/' + url.split('#')[1].split('=')[1].split('&')[0]
-    );
-} catch (error) {
-    console.log(error);
-}
