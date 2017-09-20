71815aac6be22e6f47f2b5119494da36bc857660
Zacharia <zacmwangi94@gmail.com>
2017-09-20 15:38:18 +0300

    [FIX]Fix pep8 errors

--- wger/exercises/signals.py
+++ wger/exercises/signals.py
@@ -23,9 +23,10 @@ from easy_thumbnails.files import get_thumbnailer
 from easy_thumbnails.signal_handlers import generate_aliases
 from easy_thumbnails.signals import saved_file
 
-from wger.exercises.models import ExerciseImage,Muscle
+from wger.exercises.models import ExerciseImage, Muscle
 
-@receiver(post_delete,sender=Muscle)
+
+@receiver(post_delete, sender=Muscle)
 def delete_exercise_cache_on_muscle_delete(**kwargs):
     cache.clear()
 
